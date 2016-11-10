3a6abb3a52b9eb8eae1c9468aa4fa7a2e7ddf219
Nick McNutt <nmcnutt@utk.edu>
2016-11-10 10:40:31 -0500

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,33 @@
 using Groups
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+const n = 1000
+srand(100)
+
+# Test unitary irreps for O3
+@test begin
+    for i in 1:n
+        α, β, γ = 2π*rand(), π*rand(), 2π*rand()
+        g = O3(α, β, γ)
+
+        for i in 0:4
+            unitary_irrep(g, i)
+        end
+    end
+
+    return true
+end
+
+# Test orthogonal irreps for O3
+@test begin
+    for i in 1:n
+        α, β, γ = 2π*rand(), π*rand(), 2π*rand()
+        g = O3(α, β, γ)
+
+        for i in 0:4
+            orthogonal_irrep(g, i)
+        end
+    end
+
+    return true
+end
